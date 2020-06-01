2a1290a3b09242c7bc54959acb6a800353ea434d
Johannes B <johannes.bock@live.de>
2020-06-01 22:19:59 +0200

    Projektdateien hinzufügen.

---
+++ Klausurvorbereitung_SS20/Dreieck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Klausurvorbereitung_SS20
+{
+    class Dreieck
+    {
+        double x, y, vx, vy;
+        Polygon umriss;
+        public Dreieck(Canvas Zeichenflaeche, double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            vy = 0;
+            vx = 50;
+            umriss = new Polygon();
+            umriss.Points.Add(new Point(0,20));
+            umriss.Points.Add(new Point(5,7));
+            umriss.Points.Add(new Point(-5,7));
+            umriss.Fill = Brushes.Gray;
+        }
+
+        public void Zeichne(Canvas Zeichenflaeche)
+        {
+            double winkelInGrad = Math.Atan2(vy, vx) * 180 / Math.PI -90;
+            umriss.RenderTransform = new RotateTransform(winkelInGrad);
+            Canvas.SetTop(umriss, y);
+            Canvas.SetLeft(umriss, x);
+            Zeichenflaeche.Children.Add(umriss);
+        }
+
+        public void Bewegen(TimeSpan intervall, Canvas Zeichenflaeche)
+        {
+            x += intervall.TotalSeconds * vx;
+            y += intervall.TotalSeconds * vy;
+
+            if (x < 0) x = Zeichenflaeche.ActualWidth;
+            if (x > Zeichenflaeche.ActualWidth) x = 0;
+            if (y < 0) y = Zeichenflaeche.ActualHeight;
+            if (y > Zeichenflaeche.ActualHeight) y = 0;
+        }
+
+        public void RichtungAendern(int Richtung)
+        {
+            switch (Richtung)
+            {
+                case 0:
+                    vx = -50;
+                    vy = 0;
+                    break;
+                case 1:
+                    vx = 50;
+                    vy = 0;
+                    break;
+                case 2:
+                    vx = 0;
+                    vy = -50;
+                    break;
+                case 3:
+                    vx = 0;
+                    vy = 50;
+                    break;
+            }
+        }
+    }
+}
---
+++ Klausurvorbereitung_SS20/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Klausurvorbereitung_SS20
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        List<Dreieck> Dreiecke = new List<Dreieck>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(17);
+            timer.Tick += Animiere;
+        }
+
+        private void Animiere(object sender, EventArgs e)
+        {
+            Zeichenflaeche.Children.Clear();
+            foreach (Dreieck item in Dreiecke)
+            {
+                item.Zeichne(Zeichenflaeche);
+                item.Bewegen(timer.Interval, Zeichenflaeche);
+            }
+        }
+
+        private void BTN_Start_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            BTN_Start.IsEnabled = false;
+            for (int i = 0; i < 20; i++)
+            {
+                double x, y;
+                x = Zeichenflaeche.ActualWidth / 20 * i;
+                y = Zeichenflaeche.ActualHeight / 20 * i;
+                Dreiecke.Add(new Dreieck(Zeichenflaeche, x, y));
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        Dreiecke.ForEach(x => x.RichtungAendern(0));
+                        break;
+                    case Key.Right:
+                        Dreiecke.ForEach(x => x.RichtungAendern(1));
+                        break;
+                    case Key.Up:
+                        Dreiecke.ForEach(x => x.RichtungAendern(2));
+                        break;
+                    case Key.Down:
+                        Dreiecke.ForEach(x => x.RichtungAendern(3));
+                        break;
+                }
+            }
+        }
+    }
+}
